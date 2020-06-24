da82f65b99541269d8e1d8df3bef6110ea0b2bb9
Faisal Adnan <developer@peltops.com>
2020-06-24 12:51:45 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Spring Boot JPA PostreSQL
+
+## Run Spring Boot application
+```
+mvn spring-boot:run
+```
---
+++ src/main/java/com/peltops/tutorial/repository/TutorialRepository.java
@@ -0,0 +1,12 @@
+package com.peltops.tutorial.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.peltops.tutorial.model.Tutorial;
+
+public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+	List<Tutorial> findByPublished(boolean published);
+	List<Tutorial> findByTitleContaining(String title);
+}
